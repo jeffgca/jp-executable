09de552b588ca5ad5e9b6aee09b3d7c3de70749d
canuckistani <jeff@canuckistani.ca>
2014-10-14 22:28:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Executable ( for Jetpack )
+
+This is just a quick port of [executable](https://github.com/kevva/executable) by [Kevin Mårtensson](https://github.com/kevva/) for use in Firefox extensions.
---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var fs = require('sdk/io/fs');
+var process = require('sdk/system/process');
+
+/**
+ * Check if file is executable
+ *
+ * @param {String} mode
+ * @param {String} gid
+ * @param {String} uid
+ * @api private
+ */
+
+function isExe(mode, gid, uid) {
+  var ret;
+
+  if (process.platform === 'win32') {
+    ret = true;
+  } else {
+    ret = (mode & parseInt('0001', 8)) ||
+        (mode & parseInt('0010', 8)) ||
+        (mode & parseInt('0100', 8));
+  }
+
+  return ret;
+}
+
+/**
+ * Async
+ *
+ * @param {String} name
+ * @param {Function} cb
+ * @api public
+ */
+
+module.exports = function (name, cb) {
+  fs.stat(name, function (err, stats) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    if (stats && stats.isFile() && isExe(stats.mode, stats.gid, stats.uid)) {
+      cb(null, true);
+      return;
+    }
+
+    cb(null, false);
+  });
+};
+
+/**
+ * Sync
+ *
+ * @param {String} name
+ * @api public
+ */
+
+module.exports.sync = function (name) {
+  var file = fs.statSync(name);
+
+  if (file && file.isFile() && isExe(file.mode, file.gid, file.uid)) {
+    return true;
+  }
+
+  return false;
+};
